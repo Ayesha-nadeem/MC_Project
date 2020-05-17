a1aa7fa9b850634e6eec59dcef7b57de9caf2c19
hsairashakeel <sairashakeel.py@gmail.com>
2020-05-17 04:57:29 -0400

    Splash

---
+++ app/src/main/java/pk/edu/pucit/mcproject/Splash.java
@@ -0,0 +1,31 @@
+package pk.edu.pucit.mcproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread th = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent it = new Intent(Splash.this, MainActivity.class);
+                    startActivity(it);
+                }
+            }
+        };th.start();
+    }
+
+}
